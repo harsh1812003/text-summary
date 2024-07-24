dda5a576e62315e55cdacae52d5e3ff0d65ec9d8
harsh1812003 <h9773129201@gmail.com>
2024-07-24 18:48:37 +0530

    repo

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+REPO_NAME = "text-summary"
+AUTHOR_USER_NAME = "harsh1812003"
+SRC_REPO = "text-summary"
+AUTHOR_EMAIL = "h9773129201@gmail.com"
 
 
 
